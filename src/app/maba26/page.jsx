5f37cc94cefa50dc1e1eda469c54bdabe8bf2ce6
AlfCikal <alfath.c1k4l@gmail.com>
2026-05-21 11:05:34 +0700

    aaa

--- src/app/maba26/page.jsx
+++ src/app/maba26/page.jsx
@@ -11,7 +11,7 @@ const page = () => {
 	return (
 		<div className="w-full h-screen px-8 md:px-24">
 			<h2>Hallo ini list maba ftg25</h2>
-			<div className="grid grid-cols-2 md:grid-cols-5 gap-2 w-full">
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-2 w-full">
 				{listNomor.map((n, i) => (
 					<Fotocard npm={n} angkatan={26} key={i} />
 				))}
